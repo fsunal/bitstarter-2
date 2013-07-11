991d6a40393f359df355f9a947a0b40d7bb78662
Amin Heidari <amin.heidari.88@gmail.com>
2013-07-11 20:15:06 +0000

    debugged 1

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var app = express.createServer(express.logger());
 
+var fs = require("fs");
 var indexBuff = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-	response.send(indexBuff.toString());
+	response.send(indexBuff.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
